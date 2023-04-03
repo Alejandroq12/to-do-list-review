6e70b3be77cb033bc36a97eb52d11f49d25386e6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-03 12:45:29 -0600

    Deploy site

--- src/modules/statusUpdates.js
+++
@@ -1,8 +0,0 @@
-export function toggleTaskCompletion(task, tasks) {
-  task.completed = !task.completed;
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-export function clearCompletedTasks(tasks) {
-  return tasks.filter((task) => !task.completed);
-}
--- src/modules/todo.js
+++
@@ -1,8 +0,0 @@
-import TaskElement from './taskElement.js';
-
-export const createTaskElement = (task) => {
-  const taskElement = new TaskElement(task);
-  return taskElement.create();
-};
-
-export default createTaskElement;
